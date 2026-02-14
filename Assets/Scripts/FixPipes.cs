0e88d450a3b9aa9b3a8f6ab6eca452d9c313b12e
Liam Pallett <liam.pallett06@gmail.com>
2026-02-14 23:00:35 +0000

    ...

---
+++ Assets/Scripts/FixPipes.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+public class FixPipes : MonoBehaviour
+{
+    public TilemapRenderer fixedTilemap;
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        if (playerInRange && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            //if (fixedTilemap != null) {
+                Debug.Log("Trigger");
+                fixedTilemap.enabled = true;
+            //}
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger");
+        Debug.Log("Triggered by: " + other.name + ", tag: " + other.tag);
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        { 
+            playerInRange = false;
+        }
+    }
+}
